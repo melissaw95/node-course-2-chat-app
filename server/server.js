b27f1021e2aa177202e347e3ff9a38b85b983474
Melissa Wei <melissa.weiibm.com@melissas-mbp-2.raleigh.ibm.com>
2019-02-06 14:41:50 -0500

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -1,12 +1,33 @@
-const path = require('path');
+const path = require('path'); //built in
+const http = require('http'); //built in
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(3000, () => {
   console.log(`Server is up on ${port}`);
 });
